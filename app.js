cacb268c139a1a7e3f238836840eb4c32d7b842a
Gary Lundgren <builtbygl@gmail.com>
2016-12-12 15:41:39 -0800

    added photos to img and randomly put them in html

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
- var selection = document.getElementById("selection");
-var itemsForSale = [];
+var timesViewed = 0;
+var timesSelected = 0;
+var left = document.getElementById('left');
+var right = document.getElementById('right');
+var center = document.getElementById('center');
+var selection = document.getElementById('selection');
+var itemsForSale = ['bag','banana','bathroom','boots','breakfast','bubblegum','chair','cthulhu','dog-duck','dragon','pen','pet-sweep','scissors','shark','sweep','tauntaun','unicorn','usb','water-can','wine-glass'];
+var urlPath = ['jpg','gif','png'];
+
 
 function product(productName, path) {
   this.productName = productName;
@@ -8,15 +15,23 @@ function product(productName, path) {
   this.timesSelected = 0;
   itemsForSale.push(this);
 }
+function putImageOnPage() {
+  for(var i = 0; i < 3; i++) {
+    var randomPhotoChoice = createRandomNumber();
+    console.log(randomPhotoChoice);
+    left.src = 'img/' + itemsForSale[randomPhotoChoice] + '.' + urlPath[0];
+    center.src = 'img/' + itemsForSale[randomPhotoChoice + 1] + '.' + urlPath[0];
+    right.src = 'img/' + itemsForSale[randomPhotoChoice + 2] + '.' + urlPath[0];
+  }
+}
+function createRandomNumber() {
+  var randomNumber = Math.floor(Math.random() * (itemsForSale.length - 1) + 1);
+  return randomNumber;
+}
+// createRandomNumber();
 
+putImageOnPage();
 
-// var bob = document.getElementById('bob');
-//
-// bob.addEventListener('mouseover', handleBobClick);
-//
-// function handleBobClick(event) {
-//     console.log(event.target.id);
-// }
 
 
  // ******************** Event Handler ******************************0**
