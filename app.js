c15cd7d845ecd8301bff95252a27641822771c98
Gary Lundgren <builtbygl@gmail.com>
2016-12-12 14:06:13 -0800

    set EV list to field and images

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
-var selection = document.getElementById("selection");
+ var selection = document.getElementById("selection");
+var itemsForSale = [];
+
+function product(productName, path) {
+  this.productName = productName;
+  this.path = path;
+  this.timesViewed = 0;
+  this.timesSelected = 0;
+  itemsForSale.push(this);
+}
 
 
+// var bob = document.getElementById('bob');
+//
+// bob.addEventListener('mouseover', handleBobClick);
+//
+// function handleBobClick(event) {
+//     console.log(event.target.id);
+// }
 
- // ******************** Event Handler ********************************
+
+ // ******************** Event Handler ******************************0**
  function handleSelectionSubmit(event) {
    event.preventDefault();
-   alert("You made it to step two");
+   console.log(event.target.id);
+   new product('testerMcGee', 'an url?');
  }
 
 
 // ********************* Event Listener *********************************
 selection.addEventListener("click", handleSelectionSubmit);
-
-// function(){
-//   alert("you are on step one");
-// })
