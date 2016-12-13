bb1c0f1a7492fb9c10ad5033fc81b6a2deb3367e
Gary Lundgren <builtbygl@gmail.com>
2016-12-12 17:16:50 -0800

    photos refresh

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ function putImageOnPage() {
  // ******************** Event Handler ******************************0**
  function handleSelectionSubmit(event) {
    event.preventDefault();
-   selection.innerHTML = ''
+  //  selection.innerHTML = ''
    putImageOnPage();
  }
 
@@ -49,17 +49,17 @@ function putImageOnPage() {
  new Product('breakfast', 'img/breakfast.jpg');
  new Product('bubblebum', 'img/bubblegum.jpg');
  new Product('chair', 'img/chair.jpg');
- new Product('cthulhu', 'img/cthulu.jpg');
+ new Product('cthulhu', 'img/cthulhu.jpg');
  new Product('dog-duck', 'img/dog-duck.jpg');
  new Product('dragon', 'img/dragon.jpg');
  new Product('pen', 'img/pen.jpg');
  new Product('pet-sweep', 'img/pet-sweep.jpg');
  new Product('scissors', 'img/scissors.jpg');
  new Product('shark', 'img/shark.jpg');
- new Product('sweep', 'img/sweep.jpg');
+ new Product('sweep', 'img/sweep.png');
  new Product('tauntaun', 'img/tauntaun.jpg');
  new Product('unicorn', 'img/unicorn.jpg');
- new Product('usb', 'img/usb.jpg');
+ new Product('usb', 'img/usb.gif');
  new Product('water-can', 'img/water-can.jpg');
  new Product('wine-glass', 'img/wine-glass.jpg');
 
