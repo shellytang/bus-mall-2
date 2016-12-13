055d21ff7b81123b227c7c436cc9fabd6963e8b6
Gary Lundgren <builtbygl@gmail.com>
2016-12-12 18:10:16 -0800

    photos refresh yep

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var right = document.getElementById('right');
 var center = document.getElementById('center');
 var selection = document.getElementById('selection');
 var itemsForSale = [];
+var totalClicks = 0;
+var results = document.getElementById('results');
 
 
 
@@ -22,22 +24,27 @@ function putImageOnPage() {
   var centerRandom = Math.floor(Math.random() * 20);
   var rightRandom = Math.floor(Math.random() * 20);
     left.src = itemsForSale[leftRandom].path;
+    itemsForSale[leftRandom].timesViewed += 1;
     center.src = itemsForSale[centerRandom].path;
+    itemsForSale[centerRandom].timesViewed += 1;
     right.src = itemsForSale[rightRandom].path;
+    itemsForSale[rightRandom].timesViewed += 1;
 }
-// function createRandomNumber() {
-//   var randomNumber = Math.floor(Math.random() * (itemsForSale.length - 1) + 1);
-//   return randomNumber;
-// }
+
 
  // ******************** Event Handler ******************************0**
  function handleSelectionSubmit(event) {
    event.preventDefault();
-  //  selection.innerHTML = ''
+   totalClicks += 1;
+   checkTotalClicks();
    putImageOnPage();
  }
 
-
+function checkTotalClicks() {
+  if (totalClicks > 24) {
+    selection.removeEventListener("click", handleSelectionSubmit);
+  }
+}
 
 
 //********************* instances *******************************
@@ -45,7 +52,7 @@ function putImageOnPage() {
  new Product('bag', 'img/bag.jpg');
  new Product('banana', 'img/banana.jpg');
  new Product('bathroom', 'img/bathroom.jpg');
- new Product('boots', 'img/boots.jpg');
+ new Product('boots',  'img/boots.jpg');
  new Product('breakfast', 'img/breakfast.jpg');
  new Product('bubblebum', 'img/bubblegum.jpg');
  new Product('chair', 'img/chair.jpg');
